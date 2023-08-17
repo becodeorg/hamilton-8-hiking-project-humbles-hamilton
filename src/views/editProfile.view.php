0d1500c25612cdb949d78e0fcaeb93e580648331
[Clara Mahaux] <[claramahaux@gmail.com]>
2023-08-17 17:00:59 +0200

    add views

---
+++ src/views/editProfile.view.php
@@ -0,0 +1,107 @@
+<h1>Editing</h1>
+<div class="flex justify-center">
+    <form action="#" method="post">
+        <div>
+            <label for="firstname">Your Firstname</label>
+            <input type="text" id="firstname" name="firstname" value="<?= $user['firstName']; ?>" placeholder="Firstname" required>
+        </div>
+        <div>
+            <label for="lastname">Your Lastname</label>
+            <input type="text" id="lastname" name="lastname" value="<?= $user['lastName']; ?>" placeholder="Lastname" required>
+        </div>
+        <div>
+            <label for="nickname">Your Nickname</label>
+            <input type="text" id="nickname" name="nickname" value="<?= $user['nickname']; ?>" placeholder="Nickname" required>
+        </div>
+
+        <div>
+            <label for="email">Your Email address</label>
+            <input type="email" id="email" name="email" value="<?= $user['email']; ?>" placeholder="email@example.com" required>
+        </div>
+        <div>
+            <label for="password">Your New Password</label>
+            <input type="password" id="password" name="password" placeholder="Password" required minlength="6">
+        </div>
+        <div>
+            <label for="passwordConfirm">Confirm your new password</label>
+            <input type="password" id="passwordConfirm" name="passwordConfirm" placeholder="New password confirmation" required minlength="6">
+        </div>
+
+    </form>
+</div>
+
+<div>
+    <div class="px-4 sm:px-0">
+        <h2 class="text-base font-semibold leading-7 text-gray-900">Edit your personal informations</h2>
+        </p>
+        <div class="px-4 py-6 sm:grid sm:grid-cols-3 sm:gap-4 sm:px-0">
+            <dt class="text-sm font-medium leading-6 text-gray-900">Profile picture</dt>
+            <dd class="mt-2 text-sm text-gray-900 sm:col-span-2 sm:mt-0">
+                <ul role="list" class="divide-y divide-gray-100 rounded-md border border-gray-200">
+                    <li class="flex items-center justify-between py-4 pl-4 pr-5 text-sm leading-6">
+                        <div class="flex w-0 flex-1 items-center">
+                            <svg class="h-5 w-5 flex-shrink-0 text-gray-400" viewBox="0 0 20 20" fill="currentColor" aria-hidden="true">
+                                <path fill-rule="evenodd" d="M15.621 4.379a3 3 0 00-4.242 0l-7 7a3 3 0 004.241 4.243h.001l.497-.5a.75.75 0 011.064 1.057l-.498.501-.002.002a4.5 4.5 0 01-6.364-6.364l7-7a4.5 4.5 0 016.368 6.36l-3.455 3.553A2.625 2.625 0 119.52 9.52l3.45-3.451a.75.75 0 111.061 1.06l-3.45 3.451a1.125 1.125 0 001.587 1.595l3.454-3.553a3 3 0 000-4.242z" clip-rule="evenodd" />
+                            </svg>
+                            <div class="ml-4 flex min-w-0 flex-1 gap-2">
+                                <span class="truncate font-medium">resume_back_end_developer.pdf</span>
+                                <span class="flex-shrink-0 text-gray-400">2.4mb</span>
+                            </div>
+                        </div>
+                        <div class="ml-4 flex-shrink-0">
+                            <a href="#" class="font-medium text-indigo-600 hover:text-indigo-500">Download</a>
+                        </div>
+                    </li>
+                </ul>
+        </div>
+        <div class="mt-6 border-t border-gray-100">
+            <dl class="divide-y divide-gray-100">
+                <div class="px-4 py-6 sm:grid sm:grid-cols-3 sm:gap-4 sm:px-0">
+                    <dt class="text-sm font-medium leading-6 text-gray-900">Firstname</dt>
+                    <dd class="mt-1 text-sm leading-6 text-gray-700 sm:col-span-2 sm:mt-0">
+                        <label for="firstname">Your Firstname</label>
+                        <input type="text" id="firstname" name="firstname" value="<?= $user['firstName']; ?>" placeholder="Firstname" required>
+                    </dd>
+                </div>
+                <div class="px-4 py-6 sm:grid sm:grid-cols-3 sm:gap-4 sm:px-0">
+                    <dt class="text-sm font-medium leading-6 text-gray-900">Lastname</dt>
+                    <dd class="mt-1 text-sm leading-6 text-gray-700 sm:col-span-2 sm:mt-0">
+                        <label for="lastname">Your Lastname</label>
+                        <input type="text" id="lastname" name="lastname" value="<?= $user['lastName']; ?>" placeholder="Lastname" required>
+                    </dd>
+                </div>
+                <div class="px-4 py-6 sm:grid sm:grid-cols-3 sm:gap-4 sm:px-0">
+                    <dt class="text-sm font-medium leading-6 text-gray-900">Nickname</dt>
+                    <dd class="mt-1 text-sm leading-6 text-gray-700 sm:col-span-2 sm:mt-0">
+                        <label for="nickname">Your Nickname</label>
+                        <input type="text" id="nickname" name="nickname" value="<?= $user['nickname']; ?>" placeholder="Nickname" required>
+                    </dd>
+                </div>
+                <div class="px-4 py-6 sm:grid sm:grid-cols-3 sm:gap-4 sm:px-0">
+                    <dt class="text-sm font-medium leading-6 text-gray-900">Email address</dt>
+                    <dd class="mt-1 text-sm leading-6 text-gray-700 sm:col-span-2 sm:mt-0">
+                        <label for="email">Your Email address</label>
+                        <input type="email" id="email" name="email" value="<?= $user['email']; ?>" placeholder="email@example.com" required>
+                    </dd>
+                </div>
+                <div class="px-4 py-6 sm:grid sm:grid-cols-3 sm:gap-4 sm:px-0">
+                    <dt class="text-sm font-medium leading-6 text-gray-900">Your new password</dt>
+                    <dd class="mt-1 text-sm leading-6 text-gray-700 sm:col-span-2 sm:mt-0">
+                        <label for="password">Your New Password</label>
+                        <input type="password" id="password" name="password" placeholder="Password" required minlength="6">
+                    </dd>
+                </div>
+                <div class="px-4 py-6 sm:grid sm:grid-cols-3 sm:gap-4 sm:px-0">
+                    <dt class="text-sm font-medium leading-6 text-gray-900">Your new password</dt>
+                    <dd class="mt-1 text-sm leading-6 text-gray-700 sm:col-span-2 sm:mt-0">
+                        <label for="passwordConfirm">Confirm your new password</label>
+                        <input type="password" id="passwordConfirm" name="passwordConfirm" placeholder="New password confirmation" required minlength="6">
+                    </dd>
+                </div>
+            </dl>
+        </div>
+    </div>
+    <div>
+        <a href="models/Profile.php">Cancel</a>
+        <button type="submit">Save changes</button>
+    </div>
---
+++ src/views/profile.view.php
@@ -0,0 +1,23 @@
+<div class="bg-gray-100">
+  <div class="mx-auto max-w-7xl px-4 sm:px-6 lg:px-8">
+    <div class="mx-auto max-w-2xl py-16 sm:py-24 lg:max-w-none lg:py-32">
+      <h2 class="text-2xl font-bold text-gray-900">My profile</h2>
+
+        <div class="my profile">
+          <div class="relative h-80 w-full overflow-hidden rounded-lg bg-white sm:aspect-h-1 sm:aspect-w-2 lg:aspect-h-1 lg:aspect-w-1 group-hover:opacity-75 sm:h-64">
+            <img src="" alt="User profile picture">
+          </div>
+          <h3 class="mt-6 text-sm text-gray-500">
+            <a href="#">
+              <span class="absolute inset-0"></span>
+              Nickname
+              <span class="absolute inset-0"></span>
+              Email address
+            </a>
+          </h3>
+          <p class="text-base font-semibold text-gray-900">Firstname Lastname</p>
+        </div>
+      </div>
+    </div>
+  </div>
+</div>
