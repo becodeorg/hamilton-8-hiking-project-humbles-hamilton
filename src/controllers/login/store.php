0c389f79677ae3ef86ffb2c94942dcb7319ff6fd
[Clara Mahaux] <[claramahaux@gmail.com]>
2023-08-22 13:31:54 +0200

    log changes

--- src/controllers/login/store.php
+++ src/controllers/login/store.php
@@ -1,9 +1,109 @@
 <?php
-
 session_start();
 require basePath('/core/Validator.php');
 
 $errors = [];
+
+// Check if the user is connected
+if(isset($_SESSION['user_id'])) {
+    // User is already connected, redirect to a different page
+    header('Location: /');
+    exit;
+}
+
+if ($_SERVER['REQUEST_METHOD'] === 'POST') {
+    $email = $_POST['email'];
+    $password = $_POST['password'];
+
+    if (empty($email)) {
+        $errors['email'] = 'Email is required';
+    }
+
+    if (empty($password)) {
+        $errors['password'] = 'Password is required';
+    }
+
+    if (empty($errors)) {
+        try {
+            $pdo = new PDO('mysql:host=188.166.24.55;dbname=hamilton-8-humbles');
+            $pdo->setAttribute(PDO::ATTR_ERRMODE, PDO::ERRMODE_EXCEPTION);
+        } catch (PDOException $e) {
+            die('Connection failed: ' . $e->getMessage());
+        }
+
+        $query = "SELECT * FROM `users` WHERE `email` = :email";
+        $selectStatement = $pdo->prepare($query);
+        $selectStatement->execute(['email' => $email]);
+        $user = $selectStatement->fetch(PDO::FETCH_ASSOC);
+
+        if ($user && password_verify($password, $user['password'])) {
+            $_SESSION['user_id'] = $user['id']; // Store user ID in session
+            header('Location: /');
+            exit;
+        } else {
+            $errors['login'] = 'Invalid email or password';
+        }
+    }
+}
+/*session_start();
+require basePath('/core/Validator.php');
+
+$errors = [];
+
+// Check if the user is already authenticated (connected)
+if(isset($_SESSION['user_id'])) {
+    // User is already connected, redirect to a different page
+    header('Location: /');
+    exit;
+}
+
+if ($_SERVER['REQUEST_METHOD'] === 'POST') {
+    $email = $_POST['email'];
+    $password = $_POST['password'];
+
+    if (empty($email)) {
+        $errors['email'] = 'Email is required';
+    }
+
+    if (empty($password)) {
+        $errors['password'] = 'Password is required';
+    }
+
+    if (empty($errors)) {
+        try {
+            $pdo = new PDO('mysql:host=188.166.24.55;dbname=hamilton-8-humbles', 'humbles-admin', 'xpjJMDkf1i92fY2h');
+            $pdo->setAttribute(PDO::ATTR_ERRMODE, PDO::ERRMODE_EXCEPTION);
+        } catch (PDOException $e) {
+            echo 'Connection failed: ' . $e->getMessage();
+            exit;
+        }
+        echo 'Connected to the database<br>';
+
+        $query = "SELECT * FROM `users` WHERE `email` = :email";
+        $selectStatement = $pdo->prepare($query);
+        $selectStatement->execute(['email' => $email]);
+        $user = $selectStatement->fetch(PDO::FETCH_ASSOC);
+
+        if ($user) {
+            echo 'User found<br>';
+            if (password_verify($password, $user['password'])) {
+                echo 'Password is correct<br>';
+                $_SESSION['user_id'] = $user['id']; // Store user ID in session
+                header('Location: /');
+                exit;
+            } else {
+                echo 'Password is incorrect<br>';
+                $errors['login'] = 'Invalid email or password';
+            }
+        } else {
+            echo 'User not found<br>';
+            $errors['login'] = 'Invalid email or password';
+        }
+    }
+}
+
+require basePath('views/login.view.php');
+
 
 if ($_SERVER['REQUEST_METHOD'] === 'POST') {
     $email = $_POST['email'];
@@ -52,67 +152,6 @@ if ($_SERVER['REQUEST_METHOD'] === 'POST') {
 }
 
 require basePath('views/login.view.php');
-
-/*
-
-session_start();
-require basePath('/core/Validator.php');
-
-$errors = [];
-
-// Check if the user is already authenticated (connected)
-if(isset($_SESSION['user_id'])) {
-    // User is already connected, redirect to a different page
-    header('Location: /');
-    exit;
-}
-
-if ($_SERVER['REQUEST_METHOD'] === 'POST') {
-    $email = $_POST['email'];
-    $password = $_POST['password'];
-
-    if (empty($email)) {
-        $errors['email'] = 'Email is required';
-    }
-
-    if (empty($password)) {
-        $errors['password'] = 'Password is required';
-    }
-
-    if (empty($errors)) {
-        try {
-            $pdo = new PDO('mysql:host=188.166.24.55;dbname=hamilton-8-humbles', 'humbles-admin', 'xpjJMDkf1i92fY2h');
-            $pdo->setAttribute(PDO::ATTR_ERRMODE, PDO::ERRMODE_EXCEPTION);
-        } catch (PDOException $e) {
-            echo 'Connection failed: ' . $e->getMessage();
-            exit;
-        }
-
-        echo 'Connected to the database<br>';
-
-        $query = "SELECT * FROM `users` WHERE `email` = :email";
-        $selectStatement = $pdo->prepare($query);
-        $selectStatement->execute(['email' => $email]);
-        $user = $selectStatement->fetch(PDO::FETCH_ASSOC);
-
-        if ($user) {
-            echo 'User found<br>';
-            if (password_verify($password, $user['password'])) {
-                echo 'Password is correct<br>';
-                $_SESSION['user_id'] = $user['id']; // Store user ID in session
-                header('Location: /'); // Redirect to a different page after successful login
-                exit;
-            } else {
-                echo 'Password is incorrect<br>';
-                $errors['login'] = 'Invalid email or password';
-            }
-        } else {
-            echo 'User not found<br>';
-            $errors['login'] = 'Invalid email or password';
-        }
-    }
-}
-
-require basePath('views/login.view.php');
-?>
 */
+
+
--- src/views/editProfile.view.php
+++ src/views/editProfile.view.php
@@ -9,7 +9,7 @@
         <form>
             <div class="space-y-12">
                 <div>
-                <form action="upload.php" method="POST" enctype="multipart/form-data" class="mt-10 grid grid-cols-1 gap-x-6 gap-y-8 sm:grid-cols-6">
+                <form action="#" method="POST" enctype="multipart/form-data" class="mt-10 grid grid-cols-1 gap-x-6 gap-y-8 sm:grid-cols-6">
                     <div class="col-span-full">
                         <label for="photo" class="block text-sm font-medium leading-6 text-gray-900">Profile picture</label>
                         <div class="mt-2 flex items-center gap-x-3">
@@ -65,7 +65,7 @@
                     </div>
 
                     <div class="sm:col-span-2">
-                        <label for="region" class="block text-sm font-medium leading-6 text-gray-900">New password confirmation</label>
+                        <label for="region" class="block text-sm font-medium leading-6 text-gray-900">Password confirmation</label>
                         <div class="mt-2">
                             <input type="password" id="passwordConfirm" name="passwordConfirm" required minlength="7" class="block w-full rounded-md border-0 py-1.5 text-gray-900 shadow-sm ring-1 ring-inset ring-gray-300 placeholder:text-gray-400 focus:ring-2 focus:ring-inset focus:ring-indigo-600 sm:text-sm sm:leading-6">
                         </div>
@@ -74,7 +74,7 @@
             </div>
     </div>
 </div>
-<div class="mb-6 flex items-center justify-center gap-x-6">
+<div class="mb-6 flex items-center justify-end gap-x-6">
     <a href="/profile/create" role="button" class="text-sm font-semibold leading-6 text-gray-900">Cancel</a>
     <button type="submit" class="rounded-md bg-indigo-600 px-3 py-2 text-sm font-semibold text-white shadow-sm hover:bg-indigo-500 focus-visible:outline focus-visible:outline-2 focus-visible:outline-offset-2 focus-visible:outline-indigo-600">Save</button>
 </div>
